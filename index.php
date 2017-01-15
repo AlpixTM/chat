8b8416b83df846156f2ea8fd23b69570d41ec305
AlpixTM <contact@alpix.eu>
2017-01-15 12:40:04 +0100

    Login

--- index.php
+++ index.php
@@ -20,10 +20,15 @@
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script  src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
     <style>
+
         <!-- Platz für CSS für jquery UI -->
     </style>
     <script>
+        function onSubmit (token) {
+            document.getElementById("a").submit();
+        }
         <!-- Platz für js für jquery UI -->
     </script>
 
@@ -34,28 +39,99 @@
     <header class="mdl-layout__header">
         <div class="mdl-layout__header-row">
             <!-- Title -->
-            <span class="mdl-layout-title">Title</span>
+            <span class="mdl-layout-title">Chat - Login</span>
         </div>
         <!-- Tabs -->
         <div class="mdl-layout__tab-bar mdl-js-ripple-effect">
-            <a href="#fixed-tab-1" class="mdl-layout__tab is-active">Tab 1</a>
-            <a href="#fixed-tab-2" class="mdl-layout__tab">Tab 2</a>
-            <a href="#fixed-tab-3" class="mdl-layout__tab">Tab 3</a>
+            <a href="#fixed-tab-1" class="mdl-layout__tab is-active">Login</a>
+            <a href="#fixed-tab-2" class="mdl-layout__tab">Registrieren</a>
+            <a href="#fixed-tab-3" class="mdl-layout__tab">Fehler melden</a>
         </div>
     </header>
-    <div class="mdl-layout__drawer">
-        <span class="mdl-layout-title">Chat</span>
-    </div>
+  <!--  <div class="mdl-layout__drawer">
+        <span class="mdl-layout-title">Chat - Login</span>
+    </div> -->
     <main class="mdl-layout__content">
 <!-- Haupcontent -->
         <section class="mdl-layout__tab-panel is-active" id="fixed-tab-1">
-            <div class="page-content"><!-- Your content goes here --></div>
+            <div class="page-content">
+                <!-- Login -->
+                <h3 style="text-align: center" >Login</h3>
+                <div class="mdl-grid">
+
+                    <div class="mdl-layout-spacer"></div>
+
+                          <form id="a" action="login.php" method="post">
+                                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                    <input class="mdl-textfield__input" type="text" id="Name" name="Name">
+                                    <label class="mdl-textfield__label" for="Name">Name...</label>
+                                </div>
+
+                    <div class="mdl-layout-spacer"></div>
+
+
+                    <div class="mdl-layout-spacer"></div>
+
+                        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                            <input class="mdl-textfield__input" type="text" id="pw" name="pw">
+                            <label class="mdl-textfield__label" for="pw">Passwort...</label>
+                        </div>
+                              <div class="mdl-layout-spacer"></div>
+
+                              <div class="mdl-layout-spacer"></div>
+                              <div class="mdl-grid">
+                                  <div class="mdl-layout-spacer"></div>
+                  <button class="g-recaptcha mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" data-sitekey="6LdO4REUAAAAAJ5tUklyThfRMmEN8MvCA8UlDsnQ"
+                          data-callback='onSubmit' >
+                                  Login
+                              </button>
+                                   <div class="mdl-layout-spacer"></div>
+                              </div>
+                          </form>
+
+                    <div class="mdl-layout-spacer"></div>
+
+
+
+
+                </div>
+
+
+            </div>
         </section>
         <section class="mdl-layout__tab-panel" id="fixed-tab-2">
-            <div class="page-content"><!-- Your content goes here --></div>
+            <div class="page-content">
+                <!-- Registrierung -->
+                <div class="mdl-grid">
+                    <div class="mdl-layout-spacer"></div>
+                    <div >
+
+                    </div>
+
+
+                    <div class="mdl-layout-spacer"></div>
+
+                </div>
+
+            </div>
+            </div>
         </section>
         <section class="mdl-layout__tab-panel" id="fixed-tab-3">
-            <div class="page-content"><!-- Your content goes here --></div>
+            <div class="page-content">
+                <!-- Fehler melden -->
+                <div class="mdl-grid">
+                    <div class="mdl-layout-spacer"></div>
+                    <div >
+
+                    </div>
+
+
+                    <div class="mdl-layout-spacer"></div>
+
+                </div>
+
+            </div>
+            </div>
         </section>
 
  <!-- Footer -->
