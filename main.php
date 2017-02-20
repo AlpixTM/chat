873d98ce79cc4f7b7e8844d13f5199cd48bb81db
AlpixTM <contact@alpix.eu>
2017-02-20 13:39:10 +0100

    IMG

--- main.php
+++ main.php
@@ -135,15 +135,32 @@ function show_badge_num ($num){
     </span>
   </li>
     <?php
-    $sql="SELECT `ID`,`Name` FROM `user` WHERE `status` = '1'";
+    $sql="SELECT `ID`,`Name`,`img` FROM `user` WHERE `status` = '1'";
     $db_erg = mysqli_query ( $link, $sql );
     if (! $db_erg) {
         die ( 'Ungültige Abfrage: ' . mysqli_error () );
     }
     while ($zeile = mysqli_fetch_array ( $db_erg, MYSQL_ASSOC  )) {
-      $name=$zeile['Name'];
-      echo "
+        $name=$zeile['Name'];
+        $id=$zeile['ID'];
+        $img=$zeile['img'];
+        if ($img==1) {
+            echo "
     <li class='mdl-list__item mdl-list__item--two-line'>
+        <span class='mdl-list__item-primary-content'>
+               <i class=\"material-icons mdl-list__item-avatar\"><img style=\"height: 40px; width: 40px; box-sizing: border-box; border-radius: 50%; background-color: rgb(117, 117, 117); font-size: 40px; color: rgb(255, 255, 255);\" src=\"$id.jpg\"></i>
+           <span>$name</span>
+           <span class='mdl-list__item-sub-title'>online</span>
+        </span>
+        <span class='mdl-list__item-secondary-content'>
+          <a class='mdl-list__item-secondary-action' href='#'><i class='material-icons'>info</i></a>
+        </span>
+    </li>
+";
+        }
+        else {
+            echo "
+                <li class='mdl-list__item mdl-list__item--two-line'>
         <span class='mdl-list__item-primary-content'>
            <i class='material-icons mdl-list__item-avatar'>person</i>
            <span>$name</span>
@@ -153,7 +170,9 @@ function show_badge_num ($num){
           <a class='mdl-list__item-secondary-action' href='#'><i class='material-icons'>info</i></a>
         </span>
     </li>
-";
+            ";
+        }
+
 }
 mysqli_free_result ( $db_erg );
   ?>
