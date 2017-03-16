5be395858c68e0e8dbfc842568600b8741436baf
AlpixTM <contact@alpix.eu>
2017-03-16 13:57:04 +0100

    ReST (send,connect)

---
+++ ReST/send.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: a19LukasNie
+ * Date: 13.03.2017
+ * Time: 15:05
+ */
+session_start();
+include_once 'dbconnect.php';
+if ($_SESSION['chat_sessionid']) {
+    $userid=$_SESSION['chat_userid'];
+    $sesion=$_SESSION['chat_sessionid'];
+}
+else {
+    exit("F1");
+}
+if (empty($_POST["message"]) OR empty($_POST["roomid"]) OR empty($_POST["typeid"])) {
+    exit("F2");
+}
+$message = $_POST['message'];
+$roomid= $_POST['roomid'];
+$typeid= $_POST['typeid'];
+$sql="INSERT INTO `messages` (`id`, `roomid`, `typeid`, `userid`, `txt`, `time`) VALUES (NULL, '$roomid', '$typeid', '$userid', '$message', CURRENT_TIMESTAMP);";
+$db_erg=mysqli_query($link,$sql);
+if ($db_erg){
+    echo"success";
+}
+else{
+    exit("F3");
+}
