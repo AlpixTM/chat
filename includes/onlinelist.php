85ba4b037c00573c182d928cb21f9a68b69fad0a
AlpixTM <contact@alpix.eu>
2017-05-28 20:41:49 +0200

    + Scrollen default: off

--- includes/onlinelist.php
+++ includes/onlinelist.php
@@ -30,20 +30,20 @@
 ";
         }
         else {
-            echo "
-                <li class='mdl-list__item mdl-list__item--two-line'>
+            echo "            
+    <li class='mdl-list__item mdl-list__item--two-line'>
         <span class='mdl-list__item-primary-content'>
-           <i class=\"material - icons mdl - list__item - avatar\"><img style=\"height: 40px; width: 40px; box - sizing: border - box; border - radius: 50 %; background - color: rgb(117, 117, 117); font - size: 40px; color: rgb(255, 255, 255);\" src=\"img/default.jpg\"></i>
-           <pan>$name</pan>
+               <i class=\"material-icons mdl-list__item-avatar\"><img style=\"height: 40px; width: 40px; box-sizing: border-box; border-radius: 50 %; background-color: rgb(117, 117, 117); font-size: 40px; color: rgb(255, 255, 255);\" src=\"img/default.jpg\"></i>
+           <span>$name</span>
            <span class='mdl-list__item-sub-title'>online</span>
         </span>
         <span class='mdl-list__item-secondary-content'>
           <a class='mdl-list__item-secondary-action' href='#'><i class='material-icons'>info</i></a>
         </span>
     </li>
-            ";
-        }
+";
 
+        }
     }
 mysqli_free_result ( $db_erg );
   ?>
